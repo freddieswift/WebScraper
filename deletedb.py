51f4a6b55427fa1667f5d08c1f3de153ee868f5b
freddieswift <freddie9117@live.co.uk>
2022-04-24 20:18:03 +0100

    removed db delete

--- deletedb.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient("mongodb+srv://freddieswift:NyNdZtz0lWSby7fh@cluster0.ny6zd.mongodb.net/sscDatabase?retryWrites=true&w=majority")
-db = client["sscDatabase"]
-collection = db["scrapingResult"]
-
-collection.delete_many({})
